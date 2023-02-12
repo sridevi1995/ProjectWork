6d21c0245bd7b89ac3608b904fa0c069f26601f4
sridevi1995 <sridevidurai.1995@gmail.com>
2023-02-12 16:18:29 +0530

    first time push

---
+++ src/test/java/ProjectClass/GITClass.java
@@ -0,0 +1,16 @@
+package ProjectClass;
+
+public class GITClass {
+	
+	private void java() {
+		System.out.println("1");
+	}
+	private void sql() {
+		System.out.println("2");
+
+	}
+	private void python() {
+		System.out.println("3");
+	}
+
+}
